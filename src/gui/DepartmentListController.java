641bd692cda618c5fb574dee26547082aa7db4c1
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-07-13 20:59:18 -0300

    Saving a new department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -85,6 +85,7 @@ public class DepartmentListController implements Initializable {
 
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(service);
 			controller.updateFormDate();
 
 			Stage dialogStage = new Stage();
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,8 +1,9 @@
 package model.services;
 
-import java.util.Arrays;
 import java.util.List;
 
+import gui.utils.Alerts;
+import javafx.scene.control.Alert.AlertType;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -10,7 +11,17 @@ import model.entities.Department;
 public class DepartmentService {
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 
-	public List<Department> findAll(){
+	public List<Department> findAll() {
 		return dao.findAll();
+	}
+
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+			Alerts.showAlert("SUCESSO", null, "Departamento inserido com sucesso!!", AlertType.CONFIRMATION);
+		} else {
+			dao.update(obj);
+			Alerts.showAlert("SUCESSO", null, "Departamento atualizado com sucesso!!", AlertType.CONFIRMATION);
+		}
 	}
 }
